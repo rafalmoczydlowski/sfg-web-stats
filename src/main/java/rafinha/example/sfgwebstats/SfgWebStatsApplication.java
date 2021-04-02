364b76df9b9355715cd8a60f4bc3c470e3b937b2
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-04-02 18:20:14 +0200

    Initial commit

---
+++ src/main/java/rafinha/example/sfgwebstats/SfgWebStatsApplication.java
@@ -0,0 +1,13 @@
+package rafinha.example.sfgwebstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgWebStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgWebStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rafinha/example/sfgwebstats/SfgWebStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package rafinha.example.sfgwebstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgWebStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

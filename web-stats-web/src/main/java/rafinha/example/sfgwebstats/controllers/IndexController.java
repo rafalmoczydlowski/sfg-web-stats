1c87cf563fce1b4e68851a108f7260ad1863cea8
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-04-17 11:52:34 +0200

    Add Web Stats simply index page and controller. Closes #7

---
+++ web-stats-web/src/main/java/rafinha/example/sfgwebstats/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package rafinha.example.sfgwebstats.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String getIndexPage() {
+        return "index";
+    }
+}

57fe622dc31da55666a46e24b6c13989e225dcae
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-04-23 19:26:57 +0200

    Added Coaches List and updated controller. Closes #24

--- web-stats-web/src/main/java/rafinha/example/sfgwebstats/controllers/CoachController.java
+++ web-stats-web/src/main/java/rafinha/example/sfgwebstats/controllers/CoachController.java
@@ -1,15 +1,24 @@
 package rafinha.example.sfgwebstats.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rafinha.example.sfgwebstats.services.CoachService;
 
 @RequestMapping("/coaches")
 @Controller
 public class CoachController {
 
+    private final CoachService coachService;
+
+    public CoachController(CoachService coachService) {
+        this.coachService = coachService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String getCoaches(){
+    public String getCoaches(Model model){
+        model.addAttribute("coaches", coachService.findAll());
         return "coaches/index";
     }
 }

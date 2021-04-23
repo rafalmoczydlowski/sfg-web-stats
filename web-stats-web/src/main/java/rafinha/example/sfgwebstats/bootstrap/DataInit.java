06775bc4477065323675e52adee4ac1778aa55b5
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-04-23 19:19:53 +0200

    Added Players List and updated controller. Closes #17

--- web-stats-web/src/main/java/rafinha/example/sfgwebstats/bootstrap/DataInit.java
+++ web-stats-web/src/main/java/rafinha/example/sfgwebstats/bootstrap/DataInit.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import rafinha.example.sfgwebstats.model.Club;
 import rafinha.example.sfgwebstats.model.Coach;
 import rafinha.example.sfgwebstats.model.Player;
+import rafinha.example.sfgwebstats.services.ClubService;
 import rafinha.example.sfgwebstats.services.CoachService;
 import rafinha.example.sfgwebstats.services.PlayerService;
 
@@ -13,10 +14,12 @@ public class DataInit implements CommandLineRunner {
 
     private final PlayerService playerService;
     private final CoachService coachService;
+    private final ClubService clubService;
 
-    public DataInit(PlayerService playerService, CoachService coachService) {
+    public DataInit(PlayerService playerService, CoachService coachService, ClubService clubService) {
         this.playerService = playerService;
         this.coachService = coachService;
+        this.clubService = clubService;
     }
 
     @Override
@@ -24,10 +27,14 @@ public class DataInit implements CommandLineRunner {
         Club fcBarcelona = new Club();
         fcBarcelona.setName("FC Barcelona");
 
+        clubService.save(fcBarcelona);
+
+        System.out.println("Loaded Clubs...");
+
         Player player1 = new Player();
         player1.setId(1L);
-        player1.setClub(fcBarcelona);
         player1.setPosition("GK");
+        player1.setClub(fcBarcelona);
         player1.setShirtNumber(1);
         player1.setAge(25);
         player1.setFirstName("Rafał");
@@ -36,13 +43,13 @@ public class DataInit implements CommandLineRunner {
         playerService.save(player1);
 
         Player player2 = new Player();
-        player1.setId(2L);
-        player1.setClub(fcBarcelona);
-        player1.setPosition("FW");
-        player1.setShirtNumber(10);
-        player1.setAge(33);
-        player1.setFirstName("Lionel");
-        player1.setLastName("Messi");
+        player2.setId(2L);
+        player2.setPosition("FW");
+        player2.setClub(fcBarcelona);
+        player2.setShirtNumber(10);
+        player2.setAge(33);
+        player2.setFirstName("Lionel");
+        player2.setLastName("Messi");
 
         playerService.save(player2);
 
--- web-stats-web/src/main/java/rafinha/example/sfgwebstats/controllers/PlayerController.java
+++ web-stats-web/src/main/java/rafinha/example/sfgwebstats/controllers/PlayerController.java
@@ -1,15 +1,24 @@
 package rafinha.example.sfgwebstats.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rafinha.example.sfgwebstats.services.PlayerService;
 
 @RequestMapping("/players")
 @Controller
 public class PlayerController {
 
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listPlayers() {
+    public String listPlayers(Model model) {
+        model.addAttribute("players", playerService.findAll());
         return "players/index";
     }
 }
